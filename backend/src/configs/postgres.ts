20f163b071e3124b939a5c3cfb2fbc8d56d14de4
KumaloWilson <kumalowilson900@gmail.com>
2025-03-17 13:09:39 +0200

    now moving to the extension

--- backend/src/configs/postgres.ts
+++ backend/src/configs/postgres.ts
@@ -12,7 +12,7 @@ const pool = new Pool({
   port: Number.parseInt(process.env.POSTGRES_PORT || "5432"),
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // How long a client is allowed to remain idle before being closed
-  connectionTimeoutMillis: 2000, // How long to wait for a connection to become available
+  connectionTimeoutMillis: 9000, // How long to wait for a connection to become available
 })
 
 // Log connection events for debugging
